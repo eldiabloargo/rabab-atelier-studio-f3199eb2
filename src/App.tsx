b026fe345d80bb8f7a73cff7588db3e4332caacd
eldiabloargo <eldiabloargo@gmail.com>
2026-03-13 01:57:18 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+const ProductDetail = lazy(() => import("./pages/ProductDetail"));
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -26,6 +27,7 @@ const App = () => (
         <BrowserRouter>
           <Suspense fallback={<LoadingSpinner />}>
             <Routes>
+              <Route path="/product/:id" element={<ProductDetail />} />
               <Route path="/" element={<Index />} />
               <Route path="/admin" element={<Admin />} />
               <Route path="/infos" element={<Infos />} />
