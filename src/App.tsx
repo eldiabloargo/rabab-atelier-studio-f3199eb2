847bb7506eaada3b3af6784d1e9961a22635159f
eldiabloargo <eldiabloargo@gmail.com>
2026-03-15 17:33:56 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Navbar } from ""@/components/Navbar";
+import { Navbar } from ""@/components/Navlink";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { LanguageProvider } from "@/contexts/LanguageContext";
